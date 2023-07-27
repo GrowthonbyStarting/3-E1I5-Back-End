4054b1cf1c9c273944202bf81dad675a39fc61a7
changhwan hwang <ch.hwang@ecubelabs.com>
2023-07-27 15:21:22 +0900

    feat: exception filters

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PORT } from './config';
+import { HttpExceptionFilter } from './libs/exception';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter());
+
   await app.listen(PORT);
 }
 bootstrap();
