2865bebb70eea9a1b84f4835bfd08566a9f4a57d
changhwan hwang <ch.hwang@ecubelabs.com>
2023-07-30 12:19:22 +0900

    feat: add swagger

--- src/controllers/profile.controller.ts
+++ src/controllers/profile.controller.ts
@@ -1,12 +1,15 @@
 import { Body, Controller, Post, Req } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { ProfileService } from '../services/profile.service';
 import { RegisterProfileDto } from '../dto';
 
 @Controller('/profiles')
+@ApiTags('Profile')
 export class ProfileController {
   constructor(private readonly profileService: ProfileService) {}
 
   @Post('/')
+  @ApiOperation({ summary: '프로필 생성 API', description: '프로필을 생성한다.' })
   async register(@Req() req: Request, @Body() body: RegisterProfileDto) {
     // TODO: auth guard에서 req.state.userId를 넣어준다.
     // const { userId } = req.state;
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { PORT } from './config';
 import { HttpExceptionFilter } from './libs/exception';
+import { setupSwagger } from './libs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useGlobalFilters(new HttpExceptionFilter());
+  setupSwagger(app);
 
   await app.listen(PORT);
 }
