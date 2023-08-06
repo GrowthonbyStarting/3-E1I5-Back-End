f85d4db3e1853c93ec200d809cedf29bbd7d67e3
mj-song00 <niveus128@gmail.com>
2023-08-06 15:41:33 +0900

    Chore: cors수정

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import { setupSwagger } from './libs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.enableCors({ credentials: true, origin: true });
+  app.enableCors({ credentials: true, origin: '*' });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useGlobalFilters(new HttpExceptionFilter());
   setupSwagger(app);
